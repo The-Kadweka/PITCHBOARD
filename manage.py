5efecef24c11c57418b4cf7af51f80d6e64c7ed9
hussein18149 <husseinkatana17@gmail.com>
2019-07-01 06:36:03 +0200

    made files and did the required changes

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-from app import create_app, db
+from app import create_app,db
 from app.models import User,Role
 from flask_script import Manager,Server
-from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate,MigrateCommand
 
 
 #Creating app instance
@@ -12,7 +12,7 @@ manager.add_command('db',MigrateCommand)
 
 
 @manager.shell
-def make_shell_sontext():
+def make_shell_context():
     return dict(app = app,db = db,User = User, Role = Role )
 if __name__=='__main__':
     app.secret_key = 'kadweka'
